098b01ba785c75e11add7b0291dede4e29ed107c
bpweiss <bpweiss@gmail.com>
2016-12-26 03:19:22 -0500

    Starting to add module header includes. Some syntax formatting

--- header.php
+++ header.php
@@ -1,19 +1,30 @@
-<?php
-echo '
+ 
 <!DOCTYPE html>
 <html>
 <head>
+
+
+ 
 <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <link rel="stylesheet" type"text/css" href="css/foundation.min.css">
 <script src="js/vendor/modernizr.js"></script>
 <script src="framework.js"></script> 
-<link rel="stylesheet" type"text/css" href="style.css">
+<link rel="stylesheet" type"text/css" href="style.css">'
+
+<?php 
+// if(isset($header_includes)){
+//     echo $header_includes; 
+// }
+
+
+
+echo '
 </head> 
 <body>';
 
 include('connectdb.php');
 
-include 'navigation.php';
+include ('navigation.php');
 
 ?>
