1768ad6204c99198556031b771b5d388751eae2c
bpweiss <bpweiss@gmail.com>
2016-12-26 03:31:07 -0500

    added $global_header_includes to config file, moved header resources there

--- config.php
+++ config.php
@@ -8,4 +8,15 @@ $mySqlDatabaseName="bkmkmgrone";
 $pw_salt = "MUmM_T^Inxmx8fmg";
 $pw_pepper = "cU!A_9qp";
 
+// header includes. Any global resources (css stylesheets, javascript libraries) to be added to all pages
+$global_header_includes = '<!-- load global resources -->';
+ 
+$global_header_includes .= '<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>';
+$global_header_includes .= '<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>';
+$global_header_includes .= '<link rel="stylesheet" type"text/css" href="css/foundation.min.css">';
+$global_header_includes .= '<script src="js/vendor/modernizr.js"></script>';
+$global_header_includes .= '<script src="framework.js"></script> ';
+$global_header_includes .= '<link rel="stylesheet" type"text/css" href="style.css">';
+$global_header_includes .= '<link rel="stylesheet" type="text/css" href="css/style-dailyscorecard.css">';
+
 ?>
--- header.php
+++ header.php
@@ -1,16 +1,19 @@
- 
+ <?php
+
+include ('config.php');
+
+ ?>
 <!DOCTYPE html>
 <html>
 <head>
 
+<?php
 
+echo $global_header_includes;
+
+?>
  
-<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-<link rel="stylesheet" type"text/css" href="css/foundation.min.css">
-<script src="js/vendor/modernizr.js"></script>
-<script src="framework.js"></script> 
-<link rel="stylesheet" type"text/css" href="style.css">'
+ 
 
 <?php 
 // if(isset($header_includes)){
